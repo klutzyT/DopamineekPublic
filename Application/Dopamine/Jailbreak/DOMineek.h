e7e8739732bee468a05e088da9d6b43530b225ca
klutzyT <109645532+klutzyT@users.noreply.github.com>
2025-03-13 19:04:13 -0700

    Offsets for iPhone 8,1 iOS 15.7.3
    Done to prevent rootful from being revived

--- Application/Dopamine/Jailbreak/DOMineek.h
+++ Application/Dopamine/Jailbreak/DOMineek.h
@@ -2,7 +2,7 @@
 //  DOMineek.h
 //  Dopamine
 //
-//  Created by Mineek on 26/02/2025.
+//  Created by Mineek/KlutzyT on 13/03/2025.
 //
 
 #ifndef DOMineek_h
@@ -10,21 +10,21 @@
 
 #import <Foundation/Foundation.h>
 
-// iOS 15.8.3, iPhone 6s+ (iPhone8,2)
+// iOS 15.7.3, iPhone 6s (iPhone8,1)
 // kcall
 #define ADDR_add_x0_x0_0x40_ret 0xfffffff005ac8480
 #define ADDR_getiotrap          0xfffffff007756d9c
 
 // rootful
-#define ADDR_vfs_context_kernel 0xfffffff0070ff300
-#define ADDR_vnode_lookup       0xfffffff0073216e0
-#define ADDR_vnode_lookupat     0xfffffff0073216e8
-#define ADDR_vnode_ref_ext      0xfffffff0073212d0
-#define ADDR_rootvnode_addr     0xfffffff007856730
-#define ADDR_rootvp_addr        0xfffffff0078567b0
-#define ADDR_rootdev_addr       0xfffffff00785675c
-#define ADDR_vnode_put          0xfffffff00731f240
-#define ADDR_vnode_rele         0xfffffff0073216d0
+#define ADDR_vfs_context_kernel 0xfffffff007346e94
+#define ADDR_vnode_lookup       0xfffffff0073210a0
+#define ADDR_vnode_lookupat     0xfffffff0073210a8
+#define ADDR_vnode_ref_ext      0xfffffff0073213e0
+#define ADDR_rootvnode_addr     0xfffffff007856720
+#define ADDR_rootvp_addr        0xfffffff0078567a0
+#define ADDR_rootdev_addr       0xfffffff00785674c
+#define ADDR_vnode_put          0xfffffff00731ec00
+#define ADDR_vnode_rele         0xfffffff007321090
 
 #define iOS15
 
